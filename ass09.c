a1add3e3bb2ed3e16132baf93e9539b6d647a37d
Niall Paterson <niall@piruby.com>
2012-12-06 16:51:25 +0000

    Second Commit

--- ass09.c
+++ ass09.c
@@ -1,19 +1,8 @@
 /*
+ * This program can be used as follows:
  * 
- * 
- * This program can be used in three ways:
- * 
- * a) make
- * 		This will then ask you for the re and im components
- * b) make 3 3
- * 		This will not ask you for re and im and instead do it straight away.
- * c) a.out < input.txt
- * 		Prints out results, as expected.
- * 
- * a,b holds for rotate, translate etc...
- * 
+ * a.out < inputfile.txt
  */
-
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -21,7 +10,7 @@
 #include <math.h>
 #include "mycomplex.h"
 
-void print_complex( MY_COMPLEX * z ) {
+void print_complex(MY_COMPLEX * z) {
 	// prints out complex number
 	printf("%g + %g i\n", z->re, z->im);
 }
@@ -29,73 +18,51 @@ void print_complex( MY_COMPLEX * z ) {
 void check_complex(MY_COMPLEX * z) {
 	// checks complex number exists
 	if (z == NULL) {
-		printf("You have to create a complex number before you can do anything with it");
+		printf("You have to create a complex number before you can do anything with it\n");
 		exit(-1);
 	}
 }
 
-main (int argc, char * argv[]) {
+main () {
 	// main function
-  int finished;
-  char command [ 100 ];
-  double re, im, angle,x,y;
-  MY_COMPLEX * z;
-  finished = 0;
-  while ( ! finished )
-  {
-	
-	printf("Input your command: ");
-    scanf ( "%s", command );
-    if ( strncmp ( command, "make", 6 ) == 0 ) {
-		if (sizeof(&command) != 6)
-			printf("re Component: ");
-		scanf("%lf",&re);
-		if (sizeof(&command) != 6)
-			printf("im Component: ");
-		scanf("%lf",&im);
-		z = make_complex(re,im);
-		printf("make ");
-		print_complex(z);
-    }
-    else if ( strncmp ( command, "translate", 8 ) == 0 ) {
-		check_complex(z);
-		if (sizeof(&command) != 8) {
-			printf ("Translate by:\n");
-			printf("x component: ");
+	int finished = 0;
+	char command [100];
+	double re, im, angle,x,y;
+	MY_COMPLEX * z;
+	while (!finished) {
+		scanf ("%s",command);
+		if (strncmp(command, "make",6) == 0) {
+			scanf("%lf",&re);
+			scanf("%lf",&im);
+			z = make_complex(re,im);
+			printf("make ");
+			print_complex(z);
 		}
+		else if (strncmp(command,"translate",8) == 0) {
+			check_complex(z);
 			scanf ("%lf", &x);
-		if (sizeof(&command) != 8)
-			printf("y component: ");
-		scanf ("%lf", &y);
-			
-		printf("Translate (%g,%g) ",x,y);
-		z = translate(z,x,y);
-		print_complex(z);
-    }
-    else if ( strncmp ( command, "rotate", 6 ) == 0 )
-    {
-		check_complex(z);
-		if (sizeof(&command) != 5)
-			printf("Rotate by : ");
-		scanf("%lf", &angle);
-		printf("Rotating by %g degrees ",angle);
-		z = rotate(z,angle);
-		print_complex(z);
-    }
-    else if ( strncmp ( command, "print", 5 ) == 0 )
-    {
-      check_complex(z);
-	  printf("Print ");
-	  print_complex(z);
-    }
-    else if ( strncmp ( command, "quit", 4 ) == 0 )
-      finished = 1;
-    else
-      printf ( "unrecognised command\n" );
-  }
-  printf( "Quitting\n");
+			scanf ("%lf", &y);
+			printf("translate (%g,%g) ",x,y);
+			z = translate(z,x,y);
+			print_complex(z);
+		}
+		else if (strncmp(command, "rotate",6) == 0) {
+			check_complex(z);
+			scanf("%lf", &angle);
+			printf("rotate %g degrees ",angle);
+			z = rotate(z,angle);
+			printf("result ");
+			print_complex(z);
+		}
+		else if (strncmp(command,"print",5) == 0) {
+			check_complex(z);
+			printf("print ");
+			print_complex(z);
+		}
+		else if (strncmp(command,"quit",4) == 0)
+			finished = 1;
+		else
+			printf ("unrecognised command\n");
+	}
+	printf("Quitting\n");
 }
-
-
-
-
--- mycomplex.c
+++ mycomplex.c
@@ -1,39 +1,29 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
 #include "mycomplex.h"
-// mycomplex.c
 
 MY_COMPLEX * make_complex (double re,double im) {
 	// creates a complex number in the form a+b i
-	
-	MY_COMPLEX * z;
-	z = (MY_COMPLEX *) calloc ( 1, sizeof ( MY_COMPLEX ));
+	MY_COMPLEX * z = (MY_COMPLEX *) calloc ( 1, sizeof ( MY_COMPLEX ));
 	z->re = re;
 	z->im = im;
-	
 	return z;
-	
 }
 
-MY_COMPLEX * sum(MY_COMPLEX * a, MY_COMPLEX * b) {
+MY_COMPLEX * sum (MY_COMPLEX * a, MY_COMPLEX * b) {
 	// adds two complex numbers
-	MY_COMPLEX * c = make_complex(a->re + b->re,a->im + b->im);
-	return c;
-	
+	return make_complex(a->re + b->re,a->im + b->im);
 }
 
 MY_COMPLEX * product (MY_COMPLEX * a, MY_COMPLEX * b) {
 	// multiplies two complex numbers together
-	MY_COMPLEX * c = make_complex(a->re * b->re - a->im * b->im,a->re * b->im + a->im * b->re); 
-	return c;
+	return make_complex(a->re * b->re - a->im * b->im,a->re * b->im + a->im * b->re); 
 }
 
 MY_COMPLEX * rotate (MY_COMPLEX * a,double angle) {
-	//rotates complex number by angle degress
-	MY_COMPLEX * c;
-	c = make_complex(cos(M_PI*angle/180),sin(M_PI*angle/180));
+	//rotates complex number by angle degrees
+	MY_COMPLEX * c = make_complex(cos(M_PI*angle/180),sin(M_PI*angle/180));
 	return product(a,c);
 }
 
